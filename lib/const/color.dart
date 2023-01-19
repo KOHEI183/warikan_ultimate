0cb2addc8590c2685eba1ea2b502618d5f6f6a3d
KOHEI183 <zbtbfuufp@gmail.com>
2023-01-20 00:47:30 +0900

    定数ファイルの作成 #5

---
+++ lib/const/color.dart
@@ -0,0 +1,7 @@
+// 色の定数
+import 'package:flutter/material.dart';
+
+const mainPrimaryColor = Color(0xFF0C9869); // todo:変数名とcolorコードは適当
+const mainTextColor = Color(0xFF3C4046); // todo:変数名とcolorコードは適当
+const mainBackGroundColor = Color(0xFFF9FF8FD); // todo:変数名とcolorコードは適当
+
