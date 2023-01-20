f930b74b1e6ca1abef0dbf34acf34d3f599624e6
KOHEI183 <zbtbfuufp@gmail.com>
2023-01-20 22:37:44 +0900

    @overrideの説明を記述 #12

--- lib/components/tutorial/ichiba/practice_ichiba.dart
+++ lib/components/tutorial/ichiba/practice_ichiba.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:warikan_ultimate/const/text.dart';
 import 'package:warikan_ultimate/const/color.dart';
 
+// StatelessWidgetクラスという親クラスをPracticeIchibaに継承している
 class PracticeIchiba extends StatelessWidget {
+  // Widget buildをStatelessWidgetの特性をもたせたいのでextendsしている
+  // buildメソッドもStatelessWidgetの性質をextendsされていてる
+  // StatelessWidgetは書き換えらずあくまでも、子クラスで上書きしたいため@overrideをする
   @override
   Widget build(BuildContext context) {
     return Scaffold(
